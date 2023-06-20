3e8a90bdf3b157ff76746b98d99cfb3f31678ecd
Enthony <enthonyedu@gmail.com>
2023-06-20 17:53:00 -0300

    refactor: adicionado instancias do model

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -1,12 +1,15 @@
+import 'package:somador/model/user.dart';
+
 class UserController {
   static final UserController instance = UserController();
 
-  bool login(String email, String password){
+  bool login() {
+    String email = User.instance.getEmail();
+    String password = User.instance.getPassword();
     if (email == 'admin@tsi.com.br' && password == 'master123') {
       return true;
     } else {
       return false;
     }
   }
-  
 }
